4d0c4a00407348a8d2a500870f99f1f33b7543c0
KumarManuSaraswat <kumar.saraswat.s.125@kalvium.community>
2026-05-05 17:42:31 +0530

    subset-1

---
+++ subset.py
@@ -0,0 +1,11 @@
+def subsets(nums):
+    result=[]
+    def backtrack(index,path):
+        result.append(path[:])
+        for i in range(index,len(nums)):
+            path.append(nums[i])
+            backtrack(i+1,path)
+            path.pop()
+    backtrack(0,[])
+    return result
+print(subsets([1,2,3]))
